208d6ba4fa178f5948f3ca724c0bd310ca17fe5e
Henrique <rtrans.web@gmail.com>
2017-03-16 17:54:46 +0100

    Insert all methods

--- easyDatabase.class.php
+++ easyDatabase.class.php
@@ -1,7 +1,21 @@
 <?php
 
+
+/**
+ * Class easyDatabase
+ *
+ * Cette classe permet de gérer une base de donnée MySQL avec des méthodes PHP:
+ *  - Insérer des données
+ *  - Modifier des données
+ *  - Supprimer (DANGER) des données
+ *  - Récupérer des données
+ *
+ * Licence: Apache License 2.0
+ */
+
 class easyDatabase
 {
+
     private $bdd;
 
     /**
@@ -15,9 +29,13 @@ class easyDatabase
     public function __construct($host, $nomBase, $iden, $pass)
     {
         try {
+            
             $this->setBdd(new PDO('mysql:host=' . $host . ';dbname=' . $nomBase . ';charset=utf8', $iden , $pass));
+            
         } catch (Exception $e) {
+            
             die('Erreur : ' . $e->getMessage());
+            
         }
     }
 
@@ -27,7 +45,9 @@ class easyDatabase
      */
     public function setBdd($bdd)
     {
+        
         $this->bdd = $bdd;
+        
     }
 
     /**
@@ -38,7 +58,7 @@ class easyDatabase
      * (optionnel) Passe le ou les champs à séléctionner, par exemple: 'nom, prenom'
      *
      * [@param string $where]
-     * (optionnel) Passe le ou les champs à trier, par exemple: 'nom=Henrique OR prenom=Rodrigues'
+     * (optionnel) Passe le ou les champs à trier, par exemple: 'nom="Henrique" OR prenom="Rodrigues"'
      *
      * [@param array(champs, DESC) $order]
      * (optionnel) Passe par quel champs il faut ordonner par ordre croissant, par exemple: ['nom']
@@ -55,9 +75,12 @@ class easyDatabase
      */
     public function selectAll($table, $select, $where, $order, $limit)
     {
+        
         /* check $select */
         if($select == ""){
+            
             $select = '*';
+            
         }
 
         /* build query */
@@ -65,21 +88,29 @@ class easyDatabase
 
         /* check $where and build if $where exist */
         if($where != ""){
+            
             $constQuery = $constQuery . ' WHERE ' .  $where;
+            
         }
 
         /* check $where and build if $where exist */
         if($order != ""){
+            
             $constQuery = $constQuery . ' ORDER BY ' . $order[0];
+            
             if($order[1] != ""){
+                
                 $constQuery = $constQuery . ' ' .$order[1];
+                
             }
+            
         }
 
         /* check $limit and build if $limit exist*/
         if($limit != ""){
             $constQuery = $constQuery . ' LIMIT ' .  $limit;
         }
+
 
         $reponse = $this->bdd->query($constQuery);
 
@@ -95,4 +126,74 @@ class easyDatabase
         $reponse->closeCursor();
 
     }
+
+    /**
+     * @param string $table
+     * Passe le nom de la table à utiliser
+     *
+     * @param array $values
+     * Passe les données à entrer, par exemple: ['chien', 'felix', '18]
+     */
+    public function insert($table, $values)
+    {
+        
+        $valuesContain = '';
+
+        for ( $i = 0; $i < count($values); $i++) {
+            
+            $valuesContain = $valuesContain . '"' . $values[$i] . '"';
+            
+            if( $i != (count($values)) - 1){
+                
+                $valuesContain = $valuesContain . ", ";
+                
+            }
+            
+        }
+
+        $constExec = 'INSERT INTO ' . $table . ' VALUES(' . $valuesContain . ')';
+        
+        $this->bdd->exec($constExec);
+        
+    }
+
+    /**
+     * @param $table
+     * Passe le nom de la table à utiliser
+     *
+     * @param $set
+     * Passe le ou les entrées à modifier, par exemple: 'nom="Henrique", prenom="Rodrigues"'
+     *
+     * @param $where
+     * Passe le ou les champs à modifier, par exemple: 'nom="Henrique"'
+     */
+    public function update($table, $set, $where)
+    {
+        
+        $constExec = 'UPDATE ' . $table . ' SET ' . $set . ' WHERE ' . $where;
+
+        $this->bdd->exec($constExec);
+        
+    }
+
+    /**
+     * @param string $table
+     * Passe le nom de la table à utiliser
+     *
+     * @param string $where
+     * Passe le ou les champs à supprimmer, par exemple: 'nom="Henrique"'
+     *
+     * ATTENTION MÉTHODE DANGEREUSE
+     */
+    public function delete($table, $where){
+        
+        if($where != ""){
+
+            $constExec = 'DELETE FROM ' . $table . ' WHERE ' . $where;
+
+            $this->bdd->exec($constExec);
+        }
+        
+    }
+    
 }
